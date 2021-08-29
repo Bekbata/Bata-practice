19d146c3947da6b84edf451216510d92cafaca7d
Batzorigt Sanduijav <batzorigtbek@yahoo.com>
2021-08-29 10:48:46 -0400

    more changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bata practice");
+        System.out.println("made one more changes");
         // write your code here
     }
 }
